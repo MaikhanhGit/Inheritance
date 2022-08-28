4782da3ab13bc0801c42d4d78850c6400e6d7f9c
Maikhanh <maikhanh.dsn@gmail.com>
2022-08-27 21:19:36 -0500

    Complete basic setup
    Created HealthIncrese, SpeedIncrese pickups. Created Enemy and Killer.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TankController))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] int _maxHealth = 3;
+    [SerializeField] AudioClip _killSFX;
+    int _currentHealth;
+
+    TankController _tankController;
+
+    private void Awake()
+    {
+        _tankController = GetComponent<TankController>();
+    }
+
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+    }
+
+    public void IncreaseHealth(int amount)
+    {
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, _maxHealth);        
+        _currentHealth += amount;
+        Debug.Log("Player's Health: " + _currentHealth);
+    }
+
+    public void DecreaseHealth(int amount)
+    {
+        _currentHealth -= amount;
+        Debug.Log("Player's Health: " + _currentHealth);
+        if(_currentHealth <= 0)
+        {
+            Kill();
+        }        
+    }
+
+    public void Kill()
+    {
+        gameObject.SetActive(false);
+        //play Particles
+        //play sounds
+        AudioHelper.PlayClip2D(_killSFX, 1);
+
+    }
+
+}
